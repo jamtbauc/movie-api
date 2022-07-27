46f112c83d1ed85f83fc41102d3fac525722812a
James Baucum <jamestbauc@gmail.com>
2022-07-26 20:30:19 -0500

    Remove movie db context

--- Program.cs
+++ Program.cs
@@ -3,9 +3,6 @@ using MovieApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<MovieContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("MovieContext")));
-
 builder.Services.AddControllers();
 
 var app = builder.Build();

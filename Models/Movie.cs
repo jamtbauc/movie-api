beee38e2ed27589d4f9bc36880b2fbba97861e95
James Baucum <jamestbauc@gmail.com>
2022-07-26 20:09:02 -0500

    Add movie model

---
+++ Models/Movie.cs
@@ -0,0 +1,9 @@
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool HaveWatched { get; set; }
+    }
+}
